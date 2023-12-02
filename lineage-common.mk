fea1d02081e9f14b455a9ae751e9fb68d32d8e47
Matt Filetto <matt.filetto@gmail.com>
2023-12-02 03:05:38 -0800

    add eUICC privileged permissions
    Change-Id: Idfcd124a3ccef0e69a79cf9634b8234344b0ed0d
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage-common.mk
+++ lineage-common.mk
@@ -11,3 +11,16 @@ DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lineage
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# EUICC
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zuma/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
+# Lineage Health
+include hardware/google/pixel/lineage_health/device.mk
+
+# Touch
+include hardware/google/pixel/touch/device.mk
