ccb693c8c1bdaeb8f70420d4dc28e1140913fd34
Matt Filetto <matt.filetto@gmail.com>
2024-02-20 17:57:05 -0800

    build pixeldisplayservice rro
    Change-Id: I5bc168c7744d668a218e2ad11c8c069e91d10618
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -43,6 +43,10 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
+# PixelDisplayService
+PRODUCT_PACKAGES += \
+    com.android.pixeldisplayservice.auto_generated_rro_product_adevtool__
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringOverlay
