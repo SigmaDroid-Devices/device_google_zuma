dd46736130798b50c1df9d50d2077bf8c526f6e6
Matt Filetto <matt.filetto@gmail.com>
2024-03-07 01:43:49 -0800

    zuma: drop android.hardware.security.rkp-service.citadel from vintf manifest
    * Just seems to spam logcat
    
    Change-Id: I55ebbd6e87816510c065e359de4b3bb44d6961cf
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -8,7 +8,6 @@ BUILD_BROKEN_DUP_RULES := true
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
-    device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
 
 # Partitions
