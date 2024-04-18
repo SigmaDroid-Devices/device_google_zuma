9efdccb17cb972a391bbd6ec27a8ae7bd31be0d1
Matt Filetto <matt.filetto@gmail.com>
2024-04-17 17:43:23 -0700

    zuma: add eUICC privileged permissions from stock
    Change-Id: Ie841aa30adf28a1087264b3f046547973eca12a0
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -23,6 +23,12 @@ PRODUCT_PACKAGES += \
 # Experiments
 include device/google/gs-common/performance/experiments/experiments.mk
 
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zuma/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
