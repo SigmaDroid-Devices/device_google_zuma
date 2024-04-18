82c281c9ee77ac8be384c41cc39d796cffea9924
Matt Filetto <matt.filetto@gmail.com>
2024-04-17 17:43:23 -0700

    zuma: add eUICC privileged permissions from stock
    Change-Id: Ie841aa30adf28a1087264b3f046547973eca12a0
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -16,6 +16,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# EUICC
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zuma/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
 # GMS
 ifeq ($(WITH_GMS),true)
 GMS_MAKEFILE=gms_64bit_only.mk
