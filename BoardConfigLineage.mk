5811f301097d4972ecc6e634728cfb833e73bbbc
Matt Filetto <matt.filetto@gmail.com>
2024-11-03 01:59:54 -0700

    Revert "workaround for building with prebuilt kernel headers"
    This reverts commit 1898d970149294873f36a753b352929c2fa73e43.

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,11 +5,6 @@
 #
 BUILD_BROKEN_DUP_RULES := true
 
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_KERNEL_CONFIG := cloudripper_gki_defconfig
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_KERNEL_SOURCE := kernel/google/shusky/husky
-
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
